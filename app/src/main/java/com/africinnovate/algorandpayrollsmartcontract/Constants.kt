6781b10458a9efc42469b135ad939e465bc05ebb
Glory Agatevure <agatevureglory@gmail.com>
2021-10-08 19:54:40 +0100

    updated apk

--- app/src/main/java/com/africinnovate/algorandpayrollsmartcontract/Constants.kt
+++ app/src/main/java/com/africinnovate/algorandpayrollsmartcontract/Constants.kt
@@ -6,21 +6,24 @@ object Constants {
     var EXLORER = "https://testnet.algoexplorer.io/"
     const val ALGOD_API_ADDR = "https://testnet-algorand.api.purestake.io/ps2/"
     const val ALGOD_PORT = 443
+
     const val ALGOD_API_TOKEN_KEY = "YOUR PURESTAKE API KEY"
     const val ALGOD_API_TOKEN = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
 
     //New Accounts
-//    const val ACCOUNT1_MNEMONIC = "create yours"
-//    const val ACCOUNT1_ADDRESS = "create yours"
-//
-//    const val ACCOUNT2_MNEMONIC = "create yours"
-//    const val ACCOUNT2_ADDRESS = "create yoursI"
-//
-//    const val ACCOUNT3_MNEMONIC = "create yours"
-//    const val ACCOUNT3_ADDRESS = "create yours"
-//
-//    const val ACCOUNT_MNEMONIC = "create yours"
-//    const val ACCOUNT_ADDRESS = "create yours"
+    const val ACCOUNT1_MNEMONIC = "create yours"
+    const val ACCOUNT1_ADDRESS = "create yours"
+
+    const val ACCOUNT2_MNEMONIC = "create yours"
+    const val ACCOUNT2_ADDRESS = "create yoursI"
+
+    const val ACCOUNT3_MNEMONIC = "create yours"
+    const val ACCOUNT3_ADDRESS = "create yours"
+
+    const val ACCOUNT_MNEMONIC = "create yours"
+    const val ACCOUNT_ADDRESS = "create yours"
+
+
 
     // Teal program
     val tealSource = """
